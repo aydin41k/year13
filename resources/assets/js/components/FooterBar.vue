e11b0bdf1bbc6b04877c0b8f2f83afd3c23be462
Aydin <aydin4ik@gmail.com>
2023-07-20 12:54:40 +1000

    Created a page header and moved the creds to the bottom

--- resources/assets/js/components/FooterBar.vue
+++ resources/assets/js/components/FooterBar.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="top-bar">
+    <div class="footer-bar">
         This assignment is completed by: {{ name }}
     </div>
 </template>
 
 <script>
     export default {
-        name: 'top-bar',
+        name: 'footer-bar',
         props: {
             name: {
                 default: '{ YOUR_NAME }'
@@ -16,11 +16,14 @@
 </script>
 
 <style lang="scss" scoped>
-    .top-bar {
+    .footer-bar {
         background-color: var(--brand-color);
         text-align: center;
         color: #fff;
         font-size: 0.8rem;
         padding: 3px 1rem;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
     }
 </style>
---
+++ resources/assets/js/components/Heading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="heading">
+    <Year13Logo></Year13Logo>
+    <h1>Occompare</h1>
+    <small>Compare two selected occupations in terms of Skills using ONET data</small>
+  </div>
+</template>
+
+<script>
+    import Year13Logo from './assets/Year13Logo.vue';
+    export default {
+      name: 'heading',
+      components: {Year13Logo},
+    }
+</script>
+
+<style lang="scss" scoped>
+    .heading {
+        width: 100%;
+        margin: 0 auto;
+        padding: 20px;
+        text-align: center;
+        small {
+          color: #999;
+        }
+        h1 {
+            font-size: 2.5em;
+            margin: 0 auto;
+        }
+    }
+</style>
